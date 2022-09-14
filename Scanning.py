a9a5b7d47012a01b75c043cd35fe79d6036c3b92
BOUMAHDI Abdelkrim <abdelkrim.boumahdi@gmail.com>
2022-09-14 06:55:32 +0000

    Update Scanning.py

--- Scanning.py
+++ Scanning.py
@@ -15,12 +15,15 @@ def SynScan(host):
                 except:
                     print(str(s[TCP].dport) + " IS OPEN !")
 try:
-        
-    ports = sys.argv[3].split(',')
-    ports = [eval(p) for p in ports]
-    hosts = [str (ip)  for ip in ipaddress.IPv4Network(sys.argv[1])]
+    if sys.argv[2] == '-p':
+            
+        ports = sys.argv[3].split(',')
+        ports = [eval(p) for p in ports]
+        hosts = [str (ip)  for ip in ipaddress.IPv4Network(sys.argv[1])]
 
-    for host in hosts:
-        SynScan(host)
+        for host in hosts:
+            SynScan(host)
+    else: 
+        print("Error in syntax ! example : sudo python3 Scanning.py 192.168.1.0/24 -p 80,54,45,8080 or 192.168.1.1 -p 80,54,45,8080")
 except:
     print("Error in syntax ! example : sudo python3 Scanning.py 192.168.1.0/24 -p 80,54,45,8080 or 192.168.1.1 -p 80,54,45,8080")
